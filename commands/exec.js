7e72ee54e7cf5c432870ad2f108095e438bdbf82
Lamp <ledlamp89@gmail.com>
2020-10-04 12:30:23 -0700

    split

--- commands/exec.js
+++ commands/exec.js
@@ -9,10 +9,10 @@ module.exports = class extends Akairo.Command {
 	exec (message, args) {
 		var cp = require("child_process").spawn("bash", ["-c", args.command]);
 		cp.stdout.on("data", data => {
-			message.channel.send(data.toString());
+			message.channel.send(data.toString(), {split:{char:'',length:2000}});
 		});
 		cp.stderr.on("data", data => {
-			message.channel.send(data.toString());
+			message.channel.send(data.toString(), {split:{char:'',length:2000}});
 		});
 	}
 }
