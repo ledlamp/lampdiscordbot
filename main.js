f4b63c620d1d4a27beca8d92bdce9ca9513eb1d0
Lamp <ledlamp89@gmail.com>
2021-01-22 14:22:56 -0800

    a

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ global.client = new Akairo.AkairoClient({
     ownerID: ["330499035419115522", "707359017252683896"]
 },{
     disableMentions: "everyone",
-    partials: ['MESSAGE','REACTION']
+    partials: ['MESSAGE','REACTION'],
+    fetchAllMembers: true
 })
 client.login(process.env.TOKEN).then(() => console.log("ready"))
 
