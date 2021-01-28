5f0ac9f5de16a6e565f6b9f93cec817c55fcbe68
Lamp <ledlamp89@gmail.com>
2021-01-28 14:52:19 -0800

    show attachments in pin quote, also use embedded image

--- pinboard.js
+++ pinboard.js
@@ -8,15 +8,18 @@ client.on("messageReactionAdd", async (reaction, user) => {
 			try { await reaction.fetch() } catch (e) { return }
 			try { await reaction.message.member.fetch() } catch(e) { return }
 		}
-		(await client.channels.fetch('802280618636869663'))?.send(
-			`https://discord.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id}`,
-			new Discord.MessageEmbed()
-				.setAuthor(reaction.message?.member.displayName || reaction.message.author.username, reaction.message.author.avatarURL({size:64}) || reaction.message.author.defaultAvatarURL)
-				.setDescription(reaction.message.content)
-				.setImage(reaction.message.attachments.first()?.url)
-				.setFooter(`Pinned by ${reaction.message.guild.members.resolve(user)?.displayName || user.username}`)
-				.setTimestamp(reaction.message.createdAt)
-				.setColor(reaction.message?.member.roles.color?.color)
-		);
+		let imageCandidate = reaction.message.attachments.find(a => [".png",".jpg",".jpeg",".webp",".gif"].some(e => a.url.toLowerCase().endsWith(e)));
+		if (imageCandidate) imageCandidate["will be used for the image of the embed"] = true;
+		else imageCandidate = reaction.message.embeds.find(e => e.type == 'image')?.url;
+		let embed = new Discord.MessageEmbed()
+			.setAuthor(reaction.message?.member.displayName || reaction.message.author.username, reaction.message.author.avatarURL({size:64}) || reaction.message.author.defaultAvatarURL)
+			.setDescription(reaction.message.content)
+			.setImage(reaction.message.attachments.first()?.url)
+			.setFooter(`Pinned by ${reaction.message.guild.members.resolve(user)?.displayName || user.username}`)
+			.setTimestamp(reaction.message.createdAt)
+			.setColor(reaction.message?.member.roles.color?.color);
+		let attachments = reaction.message.attachments.filter(a => !a["will be used for the image of the embed"]).map(a => `[${a.name}](${a.url})`).join('\n');
+		if (attachments) embed.addField("Attachments", attachments);
+		(await client.channels.fetch('802280618636869663'))?.send(`https://discord.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id}`, embed);
 	}
 });
