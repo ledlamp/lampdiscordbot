78e22e52accab0897935c5ca935dc740e7363c76
Lamp <ledlamp89@gmail.com>
2021-01-27 17:07:43 -0800

    Update pinboard.js

--- pinboard.js
+++ pinboard.js
@@ -4,7 +4,10 @@ client.on("messageReactionAdd", async (reaction, user) => {
 		if (reaction.message.channel.id == '802280618636869663') return;
 		if (reaction.message['has been "pinned"'] || reaction.count > 1) return;
 		reaction.message['has been "pinned"'] = true;
-		if (reaction.partial) try { await reaction.fetch() } catch (e) { return }
+		if (reaction.partial) {
+			try { await reaction.fetch() } catch (e) { return }
+			try { await reaction.message.member.fetch() } catch(e) { return }
+		}
 		(await client.channels.fetch('802280618636869663'))?.send(
 			`https://discord.com/channels/${reaction.message.guild.id}/${reaction.message.channel.id}/${reaction.message.id}`,
 			new Discord.MessageEmbed()
@@ -13,7 +16,7 @@ client.on("messageReactionAdd", async (reaction, user) => {
 				.setImage(reaction.message.attachments.first()?.url)
 				.setFooter(`Pinned by ${reaction.message.guild.members.resolve(user)?.displayName || user.username}`)
 				.setTimestamp(reaction.message.createdAt)
-				.setColor(reaction.message?.member.roles?.color.color)
+				.setColor(reaction.message?.member.roles.color?.color)
 		);
 	}
 });
