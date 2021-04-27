a34d23ed9d044c1f12a252c81e588a3a10777db3
Lamp <ledlamp89@gmail.com>
2021-04-26 23:17:49 -0700

    don't await cuz thers some gae ratelimit

--- pyrocryo.js
+++ pyrocryo.js
@@ -4,15 +4,15 @@ async function bobisfrozen() {
 }
 global.freezbob = async function freezbob() {
 	var channel = await client.channels.fetch("835734868427669574");
-	await channel.setName('❄');
-	await channel.createOverwrite(channel.guild.roles.everyone, {
+	channel.setName('❄');
+	channel.createOverwrite(channel.guild.roles.everyone, {
 		"READ_MESSAGE_HISTORY": false
 	});
 }
 global.thawbob = async function thawbob() {
 	var channel = await client.channels.fetch("835734868427669574");
-	await channel.setName('🔥');
-	await channel.createOverwrite(channel.guild.roles.everyone, {
+	channel.setName('🔥');
+	channel.createOverwrite(channel.guild.roles.everyone, {
 		"READ_MESSAGE_HISTORY": null
 	});
 }
