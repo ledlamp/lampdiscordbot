d49cc91b48caa5b25fa05a9377e02a0b4d9a132f
Lamp <ledlamp89@gmail.com>
2021-07-09 12:30:21 -0700

    INDENT USING TABS
    FUCK

--- main.js
+++ main.js
@@ -3,20 +3,20 @@ global.Akairo = require("discord-akairo")
 global.secrets = require("./secrets.json")
 
 global.client = new Akairo.AkairoClient({
-    ownerID: ["330499035419115522", "707359017252683896"]
+	ownerID: ["330499035419115522", "707359017252683896"]
 },{
-    disableMentions: "everyone",
-    partials: ['MESSAGE','REACTION'],
-    fetchAllMembers: true
+	disableMentions: "everyone",
+	partials: ['MESSAGE','REACTION'],
+	fetchAllMembers: true
 })
 client.login(secrets.TOKEN).then(() => console.log("ready"))
 
 client.commandHandler = new Akairo.CommandHandler(client, {
-    directory: "commands"
+	directory: "commands"
 })
 client.commandHandler.loadAll()
 client.listenerHandler = new Akairo.ListenerHandler(client, {
-    directory: "listeners"
+	directory: "listeners"
 })
 client.commandHandler.useListenerHandler(this.listenerHandler)
 client.listenerHandler.loadAll()
