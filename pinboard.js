90813f4a4dfbe6e4a5361df50be07681d1722fbd
Lamp <ledlamp89@gmail.com>
2021-07-09 12:53:36 -0700

    avatar cache

--- pinboard.js
+++ pinboard.js
@@ -1,9 +1,12 @@
+var fs = require("fs");
+var fetch = require("node-fetch");
 client.on("messageReactionAdd", async (reaction, user) => {
 	if (reaction.emoji.name == '📍' || reaction.emoji.name == '📌') {
 		if (!reaction.message.guild) return;
 		if (reaction.message.channel.id == '802280618636869663') return;
 		if (reaction.message['has been "pinned"'] || reaction.count > 1) return;
 		reaction.message['has been "pinned"'] = true;
+
 		if (reaction.message.channel.id == "835734868427669574") {
 			try {
 				await reaction.message.pin();
@@ -12,14 +15,36 @@ client.on("messageReactionAdd", async (reaction, user) => {
 			}
 			return;
 		}
+
 		if (reaction.partial) {
-			try { await reaction.fetch() } catch (e) { console.error("reaction fetch", e.message); return }
+			try {
+				await reaction.fetch();
+			} catch (error) {
+				console.error("reaction fetch", error.message);
+				return void await message.react('⚠');
+			}
 		}
+
+		// cache avatar because discord doesn't keep it if they change it
+		var avatarURL = reaction.message.author.avatarURL({dynamic: true});
+		if (avatarURL) {
+			let afn = avatarURL.split('/').pop();
+			let lapath = `data/www/avatars/${afn}`;
+			if (!fs.existsSync(lapath)) {
+				try {
+					(await fetch(avatarURL)).body.pipe(fs.createWriteStream(lapath));
+				} catch (error) {
+					console.error("avatar download", error.message);
+				}
+			}
+			avatarURL = `https://ldb.owo69.me/avatars/${afn}`;
+		} else avatarURL = reaction.message.author.defaultAvatarURL;
+
 		let imageCandidate = reaction.message.attachments.find(a => [".png",".jpg",".jpeg",".webp",".gif"].some(e => a.url.toLowerCase().endsWith(e)));
 		if (imageCandidate) imageCandidate["will be used for the image of the embed"] = true;
 		else imageCandidate = reaction.message.embeds.find(e => e.type == 'image');
 		let embed = new Discord.MessageEmbed()
-			.setAuthor(reaction.message.member?.displayName || reaction.message.author.username, reaction.message.author.avatarURL({size:64}) || reaction.message.author.defaultAvatarURL)
+			.setAuthor(reaction.message.member?.displayName || reaction.message.author.username, avatarURL)
 			.setDescription(reaction.message.content)
 			.setImage(imageCandidate?.url)
 			.setFooter(`Pinned by ${reaction.message.guild.members.resolve(user)?.displayName || user.username}`)
