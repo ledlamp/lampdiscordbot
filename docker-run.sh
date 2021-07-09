d8fa6c9bef2663ed32d93cd1f2df2fc23cc651ab
Lamp <ledlamp89@gmail.com>
2021-07-09 12:56:35 -0700

    Update docker run

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd `dirname "$0"`
-docker run -d --name ldb -v `pwd`:/app -w /app node:latest node main.js
+docker run -d --name ldb --restart=unless-stopped -v `pwd`:/app -w /app node:latest node main.js
